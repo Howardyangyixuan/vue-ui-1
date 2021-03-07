2ab2f767aa55572a954a8b4d9571e0b54273c265
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-07 21:28:22 +0800

    初始化官网首页和文档页面html+css，并用vue-router添加链接

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  Vite Init
-  <div>
-    <router-link to="/">howard</router-link>
-    |
-    <router-link to="/frank">frank </router-link>
-  </div>
   <router-view/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import './index.css';
+import './index.scss';
 import {createWebHashHistory, createRouter} from 'vue-router';
-import Howard from './components/Howard.vue';
-import Frank from './components/Frank.vue';
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
 
 const history = createWebHashHistory();
 const router = createRouter({
   history,
   routes: [
-    {path: '/', component: Howard},
-    {path: '/frank', component: Frank}
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc}
   ]
 });
-const app = createApp(App)
-app.use(router)
+const app = createApp(App);
+app.use(router);
 app.mount('#app');
