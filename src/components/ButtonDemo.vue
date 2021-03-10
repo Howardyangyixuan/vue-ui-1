2b6422ab287749985d64f289864895d36b9f12d1
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-10 17:22:30 +0800

    封装button，用插槽实现传值

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     ButtonDemo
+    <Button>插槽实现<Button/>
   </div>
 </template>
 
 <script lang="ts">
+  import Button from '../lib/Button.vue';
   export default {
-    name: 'ButtonDemo'
+    name: 'ButtonDemo',
+    components: {Button},
+    Component:'Button'
   };
 </script>
 
