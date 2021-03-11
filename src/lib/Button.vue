cd16c4b6c6f7ea6666ad470a93983e5311253247
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-11 23:24:16 +0800

    添加level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -20,16 +20,21 @@
       size: {
         type: String,
         default: 'normal'
+      },
+      level: {
+        type: String,
+        default: 'normal',
       }
     },
     setup(props, context) {
       const {style, ...attrs} = context.attrs;
-      const {theme, size} = props;
-      console.log({[`vue-ui-theme-${theme}`]: theme})
+      const {theme, size, level} = props;
+      console.log({[`vue-ui-theme-${theme}`]: theme});
       const classes = computed(() => {
         return {
           [`vue-ui-theme-${theme}`]: theme,
           [`vue-ui-size-${size}`]: size,
+          [`vue-ui-level-${level}`]: level,
         };
       });
       return {
@@ -45,6 +50,7 @@
   $border-color: #d9d9d9;
   $color: #333;
   $blue: #40a9ff;
+  $red: red;
   $radius: 4px;
   .vue-ui-button {
     box-sizing: border-box;
@@ -60,6 +66,7 @@
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
     margin-left: 8px;
 
     &:hover,
@@ -101,10 +108,68 @@
       height: 48px;
       padding: 0 16px;
     }
+
     &.vue-ui-size-small {
       font-size: 12px;
       height: 20px;
       padding: 0 4px;
+    }
+
+    &.vue-ui-theme-button {
+      &.vue-ui-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
+      }
+
+      &.vue-ui-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.vue-ui-theme-link {
+      &.vue-ui-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+
+    &.vue-ui-theme-text {
+      &.vue-ui-level-main {
+        color: $blue;
+
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+
+      &.vue-ui-level-danger {
+        color: $red;
+
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
     }
   }
 </style>
