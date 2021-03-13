d6cccd48ea7b35ce0293053b4edbe22335649d02
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-13 20:49:13 +0800

    修复sass的bug

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    DialogDemo
+    <div>Dialog 示例</div>
+    <h1>示例1</h1>
+    <Dialog/>
   </div>
 </template>
 
 <script lang="ts">
+  import Dialog from '../lib/Dialog.vue';
+
   export default {
-    name: 'DialogDemo'
+    name: 'DialogDemo',
+    components: {Dialog}
   };
 </script>
 
