da0bcb05cb5ef21d11161e73ece68624e67afd27
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-22 11:13:13 +0800

    继续铜鼓slots中得到的内容+slot嵌套渲染子组件

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    Tabs
-    <component :is="defaults[0]" />
-    <component :is="defaults[1]" />
-  </div>
+    <div v-for="(title,idx) in titles" :key="idx">{{title}}</div>
+    <component v-for="(d,idx) in defaults" :key="idx" :is="d"/>
 </template>
 
 <script lang="ts">
@@ -11,17 +9,21 @@
 
   export default {
     name: 'Tabs',
-    setup(props, context){
-      const defaults = context.slots.default()
-      console.log(defaults[0]);
-      defaults.forEach((item)=>{
-        if(item.type !== Tab){
-          throw new Error("slots中存在类型不是Tab的标签!")
+    setup(props, context) {
+      const defaults = context.slots.default();
+      defaults.forEach((item) => {
+        if (item.type !== Tab) {
+          throw new Error('slots中存在类型不是Tab的标签!');
         }
-      })
-      return{
-        defaults
-      }
+      });
+      const titles = defaults.map((item) => {
+        return item.props.title;
+      });
+      console.log(titles);
+      return {
+        defaults,
+        titles
+      };
     }
   };
 </script>
