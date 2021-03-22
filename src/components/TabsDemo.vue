73fb615bd7a4b7dddcd12ccb4675a0044287b692
howardyangyixuan <howardyangyixuan@gmail.com>
2021-03-22 10:56:18 +0800

    添加Tabs和Tab组件，并通过slots.default()确保Tabs中均为Tab

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,11 +2,19 @@
   <div>
     TabsDemo
   </div>
+  <h1>示例一</h1>
+  <Tabs>
+    <Tab title="导航一">内容一</Tab>
+    <Tab title="导航二">内容二</Tab>
+  </Tabs>
 </template>
 
 <script lang="ts">
+  import Tabs from '../lib/Tabs.vue';
+  import Tab from '../lib/Tab.vue';
   export default {
-    name: 'TabsDemo'
+    name: 'TabsDemo',
+    components: {Tab, Tabs}
   };
 </script>
 
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Tabs
+    <component :is="defaults[0]" />
+    <component :is="defaults[1]" />
+  </div>
+</template>
+
+<script lang="ts">
+  import Tab from './Tab.vue';
+
+  export default {
+    name: 'Tabs',
+    setup(props, context){
+      const defaults = context.slots.default()
+      console.log(defaults[0]);
+      defaults.forEach((item)=>{
+        if(item.type !== Tab){
+          throw new Error("slots中存在类型不是Tab的标签!")
+        }
+      })
+      return{
+        defaults
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
